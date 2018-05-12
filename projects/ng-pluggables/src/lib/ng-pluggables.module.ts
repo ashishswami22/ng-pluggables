8b0b95c525a8b00ce81d1c861a01e199946aaad2
Ashish <ashishswami22@gmail.com>
2018-05-12 23:21:20 +0530

    Added digital clock component

--- projects/ng-pluggables/src/lib/ng-pluggables.module.ts
+++ projects/ng-pluggables/src/lib/ng-pluggables.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { NgPluggablesComponent } from './ng-pluggables.component';
+import { AnalogClockComponent } from './components/analog-clock/analog-clock.component';
 
 @NgModule({
   imports: [
   ],
-  declarations: [NgPluggablesComponent],
-  exports: [NgPluggablesComponent]
+  declarations: [NgPluggablesComponent, AnalogClockComponent],
+  exports: [NgPluggablesComponent, AnalogClockComponent]
 })
 export class NgPluggablesModule { }
